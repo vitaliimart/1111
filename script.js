15323a5bf648f30e6eca0cd452a61dad351b9a8b
Vitalik <example@gmail.com>
2021-05-08 14:28:58 +0300

    практика. часть 1 - создание объекта и его частичное наполение

--- script.js
+++ script.js
@@ -1,67 +1,39 @@
-// let myName = `Alex`;
+/* 
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос: 
+"Сколько фильмов Вы уже посмотрели?"
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+count - сюда передается ответ на первый вопрос;
+movies - в это свойство поместить пустой объект;
+actors - тоже поместить пустой объект;
+genres - суда поместить пустой массив;
+privat - в это свойство поместиь boolean (логическое) значение false;
 
-// let bool = true;
+3) Задайте пользователю по два раза вопросы:
+"Один из последних просмотренных фильмов?"
+"На сколько оцените его?"
 
-// let modal;
-// console.log(modal);
-// console.log(myName);
-// let obj = {
-// 	dog: 'Puppy',
-// 	cat: 'Murr'
-// };
+Ответы стоит посместить в отдельные переменные 
+Записать ответы в объект movies в формате:
+movies: {
+	"logan": "8.1"
+}
+*/
 
-// let arr = [1, 'str', false];
-// arr[0] = 2;
+const numberOfFilms = prompt("Сколько фильмов Вы уже посмотрели?", "");
 
-// console.log(4/0);
-// console.log(-4/0);
-// console.log('someText'/0);
-// console.log('someText'-0);
-// console.log("2" + '2');
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = prompt("На сколько оцените его?", "");
 
-// let x = 32;
-// x--;
-// console.log(x);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-// alert(x);
-
-// let answer = confirm('Do you have 18 years?');
-// console.log(answer);
-
-// let answer = prompt('How old are you?', '');
-// console.log(answer);
-
-// let answers = [];
-
-// answers[0] = prompt('What is your name?', '');
-// answers[1] = prompt('What is your surname?', '');
-// answers[2] = prompt('How old are you?', '');
-// document.write(answers);
-
-
-
-// function myFirstApp(name, age) {
-// 	alert (`Hello, my name is ${name} and it is my first App!`);
-
-// 	function showSkills() {
-// 		let skills = ['html','css','js'];
-// 		alert (`I am using: ${skills}`);
-// 	}
-// 	showSkills();
-
-// 	function chackAge() {
-// 		if (age > 18) {
-// 			alert("Good");
-// 		} else {
-// 			alert("Bad");
-// 		}
-// 	}
-// 	chackAge();
-
-// 	function calcPow(num)  {
-// 		let pow = num*num;
-// 		return pow;
-// 	}
-// 	console.log(calcPow(40));
-// }
-// myFirstApp("Ivan", 19);
+console.log(personalMovieDB);
