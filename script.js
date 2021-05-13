e80dbbf60b4a3c20c54f1f480d375c0284c7a497
Vitalik <example@gmail.com>
2021-05-13 22:29:39 +0300

    modified js-file

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 /* 
 1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос: 
 "Сколько фильмов Вы уже посмотрели?"
@@ -20,21 +21,53 @@ movies: {
 Проверить выполнение кода через console.log
 */
 
-const numberOfFilms = prompt("Сколько фильмов Вы уже посмотрели?", "");
+// const numberOfFilms = prompt("Сколько фильмов Вы уже посмотрели?", "");
 
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
-};
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = prompt("На сколько оцените его?", ""),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = prompt("На сколько оцените его?", "");
+// const personalMovieDB = {
+// 	count: numberOfFilms,
+// 	movies: {},
+// 	actors: {},
+// 	genres: [],
+// 	privat: false
+// };
+// const a = prompt("Один из последних просмотренных фильмов?", ""),
+//       b = prompt("На сколько оцените его?", ""),
+//       c = prompt("Один из последних просмотренных фильмов?", ""),
+//       d = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+//-------------------------------------------------------------------------------------------------------------------
+let number = 4.6;
+console.log(4/4);
+console.log(4/0);
+console.log(-4/0);
+console.log("aaaa"/0);
+console.log(0/"aaaa");
+console.log("dgfgfg"/"aaaa");
+console.log("2"/"2");
+console.log("2"/"0");
+console.log(""/"2");
+
+const personel ="Alex";
+console.log(personel);
+
+// let weddf;
+// console.log(weddf);
+
+// const obj = {
+// 	name: "Alex",
+// 	surname: "Smith",
+// 	age: 32,
+// 	isMarried: true
+// };
+// console.log(obj["name"]);
+// console.log(obj["age"]);
+// console.log(typeof(obj["age"]));
+// console.log(typeof(obj["isMarried"]));
+
+const arr = ["plum.png", {a: 1, b: "str", c: true}, 23, [true, false]];
+console.log(arr[1]);
