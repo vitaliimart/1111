ca27c28efe0c52788022445935a78892e89e715f
Vitalik <example@gmail.com>
2021-06-09 21:54:14 +0300

    Массивы и псевдомассивы

--- script.js
+++ script.js
@@ -230,42 +230,86 @@ genres
 // }
 
 // learnJJ("JavaScript", done);
-"use strict";
 
-const options = {
-	name: "test",
-	width: 1024,
-	height: 1024,
-	colors: {
-		border: "black",
-		bg: "red"
-	},
-	makeTest: function() {
-		console.log("Some text");
-	}
-};
-options.makeTest();
+// const options = {
+// 	name: "test",
+// 	width: 1024,
+// 	height: 1024,
+// 	colors: {
+// 		border: "black",
+// 		bg: "red"
+// 	},
+// 	makeTest: function() {
+// 		console.log("Some text");
+// 	}
+// };
+// options.makeTest();
 
-const {border, bg} = options.colors;
-console.log(border);
+// const {border, bg} = options.colors;
+// console.log(border);
 
-console.log(options.name);
-delete options.name;
-console.log(options);
-console.log(options['colors']['border']);
-let counter = 0;
-for (let key in options) {
-	if (typeof(options[key]) === 'object') {
-		for (let i in options[key]){
-			console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-			// counter++;
-		}
-	} else {
-		console.log(`Свойство ${key} имеет значение ${options[key]}`);
-		counter++;
-	}
-}
+// console.log(options.name);
+// delete options.name;
+// console.log(options);
+// console.log(options['colors']['border']);
+// let counter = 0;
+// for (let key in options) {
+// 	if (typeof(options[key]) === 'object') {
+// 		for (let i in options[key]){
+// 			console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+// 			// counter++;
+// 		}
+// 	} else {
+// 		console.log(`Свойство ${key} имеет значение ${options[key]}`);
+// 		counter++;
+// 	}
+// }
 
-console.log(counter);
-console.log(Object.keys(options));
-console.log(Object.keys(options).length);
+// console.log(counter);
+// console.log(Object.keys(options));
+// console.log(Object.keys(options).length);
+// "use strict";
+
+// const arr = [2, 13, 8, 26, 10];
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a, b) {
+// 	return a - b;
+// }
+
+// arr.pop();
+// arr.push(10);
+
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+// 	console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+// 	console.log(value);
+// }
+
+// console.log(arr.length);
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr);
+
+// arr.forEach(function(item, i, arr) {
+// 	console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+// for (let value of arr) {
+// 	console.log(value);
+// }
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+
+// products.sort();
+
+// console.log(products);
+
+// console.log(products.join("; "));
+
