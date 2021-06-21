5c980feb546cad57e3a47b6bca306930e1e58831
Vitalik <example@gmail.com>
2021-06-21 14:27:26 +0300

    Practice#4

--- js/script.js
+++ js/script.js
@@ -592,7 +592,7 @@ console.log(typeof(!"444"));
 
 console.log(typeof(! 444));
 */
-
+/*
 let z = 5;
 
 console.log(z++); // 5
@@ -645,3 +645,69 @@ console.log(one.length > two.length); // false
 console.log("Ёжик"> "яблоко"); // false
 
 console.log(0 || "" || 2 || undefined || true || false); // 2
+*/
+
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+// 1) Удалить все рекламные блоки со страницы (правая часть сайта)
+const adv = document.querySelectorAll(".promo__adv img"),
+      poster = document.querySelector(".promo__bg"),
+      genre = poster.querySelector(".promo__genre"),
+		movieList = document.querySelector(".promo__interactive-list");
+
+// первый метод удаления рекламных блоков
+adv.forEach(item => {
+	item.remove();
+});
+
+// второй метод удаления рекламных блоков
+// adv.forEach(function (item) {
+// 	item.remove();
+// });
+
+// 2) Изменить жанр фильма, поменять "комедия" на "драма"
+genre.textContent = "драма";
+
+/* 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img. 
+      Реализовать только при помощи JS */
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+// 4) Список фильмов на странице сформировать на основании данных из этого JS файла. Отсортировать их по алфавиту
+// 5) Добавить нумерацию выведенных фильмов */
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+
+// console.log(poster.innerHTML);
+movieDB.movies.forEach((film, i) => {
+	movieList.innerHTML += `
+	   <li class="promo__interactive-item">${i + 1} ${film}
+	      <div class="delete"></div>
+      </li>
+	`;
+});
+
